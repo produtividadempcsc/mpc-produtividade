10d3b6b4d83614ea5092c82065c7ce1ed008d7a6
produtividadempcsc <produtividadempc@gmail.com>
2026-08-13 00:11:46 -0300

    Delete forms directory

--- forms/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .processo import display_edit_processo_form  # noqa: F401
-from .actions import display_analise_form, display_chefe_update_form, display_servidor_update_form  # noqa: F401
-from .admin import display_admin_edit_processo_form, display_edit_prompt_form  # noqa: F401
-from utils.ui import display_process_history  # noqa: F401
